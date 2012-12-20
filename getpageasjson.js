f0ab739f49d4f5e57b5321470f3606583f58b5f9
Dhruv <dhruvbird@gmail.com>
2012-12-19 20:41:09 -0500

    Print page details as json.

--- getpageasjson.js
+++ getpageasjson.js
@@ -34,14 +34,14 @@ function multi_get_urls(urls, cb) {
     }
 }
 
-function get_main_page_titles(cb) {
-    util.getURL(URL, function(body, err) {
+function get_main_page_titles(url, cb) {
+    util.getURL(url, function(body, err) {
         if (err) {
             console.error(err);
             return;
         }
 
-        console.log("GOT PAGE");
+        // console.error("GOT PAGE");
 
         util.loadDOM(body, function($, window, errors) {
             var main_page_links = $("td.MainPageBG b a").filter(function(i, x) {
@@ -59,11 +59,7 @@ function get_main_page_titles(cb) {
             main_page_titles = _.uniq(main_page_titles, false, function(link) {
                 return link.href;
             });
-            main_page_titles = main_page_titles.map(function(title) {
-                title['abstract'] = "Lorem Ipsum...";
-                return title;
-            });
-            console.log(main_page_titles);
+            // console.error(main_page_titles);
             cb(main_page_titles);
         });
     });
@@ -106,14 +102,39 @@ function parse_content(content) {
 }
 
 function main() {
-    get_main_page_titles(function(main_page_titles) {
-        var main_mage_urls = main_page_titles.map(function(title) {
+    var opts = require('tav').set({
+        'url': {
+            note: 'The URL of the page to scrape links from (default: [empty])',
+            value: ''
+        }
+    }, 'Fetch interesting links from a wikipedia page given the URL of that page');
+
+    if (!opts.url) {
+        console.error("You MUST specify the URL using the --url=<URL> option.");
+        return;
+    }
+
+    function get_page_contents(page_titles) {
+        var page_urls = page_titles.map(function(title) {
             return "http://en.wikipedia.org" + title.href;
         }).slice(0, 10);
         // multi_get_urls(main_mage_urls, print_content);
-        multi_get_urls(main_mage_urls, parse_content);
-    });
+        multi_get_urls(page_urls, parse_content);
+    }
+
+    function print_titles_as_JSON(page_titles) {
+        var exit_code = 0;
+        try {
+            var j = JSON.stringify(page_titles, true, '  ');
+            console.log(j);
+        } catch (ex) {
+            console.error(ex.stack);
+            exit_code = 1;
+        }
+        process.exit(exit_code);
+    }
+
+    get_main_page_titles(opts.url, print_titles_as_JSON);
 }
 
 main();
-setInterval(function(){}, 1000);

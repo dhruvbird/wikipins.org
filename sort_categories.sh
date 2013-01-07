390190b1ae3a8e9c7e9c3e434a52bf5650e960dd
Dhruv <dhruvbird@gmail.com>
2013-01-07 02:49:05 -0500

    Set back to C and include reference.

--- sort_categories.sh
+++ sort_categories.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 TAB=`printf "\t"`
-export LANG=en_US
-export LC_ALL=en_US
+# http://www.madboa.com/geek/utf8/
+export LANG=C
+export LC_ALL=C
 sort -f -k 1 -t "$TAB" -S 400M category.tsv > category.category.sorted.tsv
 sort -f -k 2 -t "$TAB" -S 400M category.tsv > category.title.sorted.tsv

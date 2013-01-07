fcb0f556d7b21833f4324143a85568c142e71c90
Dhruv <dhruvbird@gmail.com>
2013-01-07 02:46:18 -0500

    Set LANG to en_US.

--- sort_categories.sh
+++ sort_categories.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 TAB=`printf "\t"`
-export LANG=C
+export LANG=en_US
+export LC_ALL=en_US
 sort -f -k 1 -t "$TAB" -S 400M category.tsv > category.category.sorted.tsv
 sort -f -k 2 -t "$TAB" -S 400M category.tsv > category.title.sorted.tsv

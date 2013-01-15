61f9440dbe2edc963fdcd2e12ce16fd4638cf15a
Dhruv <dhruvbird@gmail.com>
2013-01-15 05:40:35 -0500

    Added serve.js.

---
+++ serve.js
@@ -0,0 +1,57 @@
+var http    = require('http');
+var express = require('express');
+var app     = express();
+var ds      = require("./datastore.js");
+
+app.set("jsonp callback", true);
+
+// /random_categories/
+app.get("/random_categories[/]?", function(req, res) {
+    ds.get_random_category_images(function(category_images) {
+        res.jsonp(category_images);
+    });
+});
+
+// /suggest_categories/?category=PREFIX
+app.get("/suggest_categories[/]?", function(req, res) {
+    var category = req.query.category;
+    if (!category) {
+        res.send("[]");
+        return;
+    }
+    ds.suggest_categories(category, 10, function(suggested_categories) {
+        res.jsonp(suggested_categories);
+    });
+});
+
+// Fetch the list of titles & abstracts for a given category name.
+//
+// /category_abstracts/?category=CATEGORY_NAME
+app.get("/category_abstracts[/]?", function(req, res) {
+    var category = req.query.category;
+    if (!category) {
+        res.send("[]");
+        return;
+    }
+    ds.get_category_titles(category, function(titles) {
+        ds.get_multi_abstracts_by_title(titles, function(abstracts) {
+            res.jsonp(abstracts);
+        });
+    });
+});
+
+// Fetch related categories for a given category name
+//
+// /related_categories/?category=CATEGORY_NAME
+app.get("/related_categories[/]?", function(req, res) {
+    var category = req.query.category;
+    if (!category) {
+        res.send("[]");
+        return;
+    }
+    ds.get_related_categories(category, function(related_categories) {
+        res.jsonp(abstracts);
+    });
+});
+
+app.listen(8080);

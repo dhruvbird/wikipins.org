6ac6fcc8d1713284690f8ca860aae3dba11a8729
Dhruv <dhruvbird@gmail.com>
2013-01-21 03:04:19 -0500

    Added js script to clean category hit log.

---
+++ clean_hit_log.js
@@ -0,0 +1,37 @@
+/* Clean wikipedia hit logs.
+ *
+ */
+
+var fs      = require('fs');
+var util    = require("util");
+var events  = require("events");
+var carrier = require('carrier');
+
+function main() {
+    var opts = require('tav').set({
+        'type': {
+            note: 'The type of hit logs. Values: (category|article) (default: category)',
+            value: 'category' // This is currently unused.
+        }
+    }, 'Clean wikipedia hit logs');
+
+    process.stdin.resume();
+    var fin = process.stdin;
+    var lin = carrier.carry(fin, on_got_line);
+    var splitRE = /\s+/g;
+    var cprefixRE = /^\s*category:/i;
+
+    function on_got_line(line) {
+        var parts = line.split(splitRE);
+        var title = parts[1];
+        var hits  = parts[2]
+        var b = new Buffer(unescape(title), 'binary');
+        title = b.toString('utf8');
+        title = title.replace(cprefixRE, '').trim();
+        title = title.replace(/_/g, ' ');
+        process.stdout.write(title + "\t" + hits + "\n");
+    }
+
+}
+
+main();

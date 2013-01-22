b60d1f8eec831bf2be35ae5a9be7bfeff26e3d88
Dhruv <dhruvbird@gmail.com>
2013-01-22 02:55:58 -0500

    Add command line options to serve.js and generally refactor the code.

--- serve.js
+++ serve.js
@@ -4,63 +4,79 @@ var fs      = require("fs");
 var app     = express();
 var ds      = require("./datastore.js");
 
-app.set("jsonp callback", true);
-
-// /random_categories/
-app.get("/random_categories[/]?", function(req, res) {
-    ds.get_random_category_images(function(category_images) {
-        res.jsonp(category_images);
-    });
-});
-
-// Fetch the list of titles & abstracts for a given category name.
-//
-// /category_abstracts/?category=CATEGORY_NAME
-app.get("/category_abstracts[/]?", function(req, res) {
-    var category = unescape(req.query.category);
-    if (!category) {
-        res.send("[]");
-        return;
-    }
-    ds.get_category_titles(category, function(titles) {
-        ds.get_multi_abstracts_by_title(titles, function(abstracts) {
-            res.jsonp(abstracts);
-        });
-    });
-});
-
-// Fetch related categories for a given category name
-//
-// /related_categories/?category=CATEGORY_NAME
-app.get("/related_categories[/]?", function(req, res) {
-    var category = unescape(req.query.category);
-    if (!category) {
-        res.send("[]");
-        return;
-    }
-    ds.get_related_categories(category, function(related_categories) {
-        res.jsonp(abstracts);
-    });
-});
-
 function serve_static_file(req, res, file_path) {
     var index_path = require.resolve(file_path);
     var index_fd = fs.openSync(index_path, 'r');
     var index_stream = fs.createReadStream(index_path, {
         fd: index_fd,
         encoding: 'utf8'
-    });
+        });
     index_stream.pipe(res);
 }
 
-app.use('/static/', express.static(__dirname + "/static/"));
+function main() {
+    var opts = require('tav').set({
+        'db': {
+            note: 'The database name to use for querying (default: wikipins)',
+            value: 'wikipins'
+        },
+        'port': {
+            note: 'The port on which to listen for connections (default: 80)',
+            value: 80
+        }
+    }, 'Serve the wikipins.org site');
 
-app.get("/", function(req, res) {
-    serve_static_file(req, res, "./static/index.html");
-});
+    app.set("jsonp callback", true);
 
-app.get("/c/[^/]+[/]?", function(req, res) {
-    serve_static_file(req, res, "./static/index.html");
-});
+    // /random_categories/
+    app.get("/random_categories[/]?", function(req, res) {
+        ds.get_random_category_images(function(category_images) {
+            res.jsonp(category_images);
+        });
+    });
 
-app.listen(8080);
+    // Fetch the list of titles & abstracts for a given category name.
+    //
+    // /category_abstracts/?category=CATEGORY_NAME
+    app.get("/category_abstracts[/]?", function(req, res) {
+        var category = unescape(req.query.category);
+        if (!category) {
+            res.send("[]");
+            return;
+        }
+        ds.get_category_titles(category, function(titles) {
+            ds.get_multi_abstracts_by_title(titles, function(abstracts) {
+                res.jsonp(abstracts);
+            });
+        });
+    });
+    
+    // Fetch related categories for a given category name
+    //
+    // /related_categories/?category=CATEGORY_NAME
+    app.get("/related_categories[/]?", function(req, res) {
+        var category = unescape(req.query.category);
+        if (!category) {
+            res.send("[]");
+        return;
+        }
+        ds.get_related_categories(category, function(related_categories) {
+            res.jsonp(abstracts);
+        });
+    });
+    
+    app.use('/static/', express.static(__dirname + "/static/"));
+
+    app.get("/", function(req, res) {
+        serve_static_file(req, res, "./static/index.html");
+    });
+    
+    app.get("/c/[^/]+[/]?", function(req, res) {
+        serve_static_file(req, res, "./static/index.html");
+    });
+
+    ds.set_db_name(opts.db);
+    app.listen(opts.port);
+}
+
+main();

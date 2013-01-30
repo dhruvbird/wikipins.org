7b00403e5b6dadf3a4730fa2b4804428ba31c8b6
Dhruv <dhruvbird@gmail.com>
2013-01-30 03:27:02 -0500

    Use ejs for templating and provide a custom 404 page. Thanks @ved_a.

--- serve.js
+++ serve.js
@@ -1,10 +1,14 @@
 var http    = require('http');
 var express = require('express');
 var fs      = require("fs");
+var ejs     = require('ejs');
+var path    = require('path');
 var app     = express();
 var ds      = require("./datastore.js");
 
 var spiderUARE = /facebookexternalhit|googlebot/i;
+var i404 = [ ];
+var i404_idx = 0;
 
 function serve_static_file(req, res, file_path, encoding) {
     encoding = encoding || 'utf8';
@@ -31,6 +35,13 @@ function main() {
 
     console.error("[%s] Starting the wikipins.org serving engine on port '%s' & db '%s'",
                   String(new Date()), String(opts.port), String(opts.db));
+
+    var dir_404 = path.dirname(require.resolve("./static/404/404.html"));
+    i404 = fs.readdirSync(dir_404).filter(function(iname) {
+        return iname.search(/\.jpg$/i) != -1;
+    });
+
+    template_404 = ejs.compile(fs.readFileSync(require.resolve("./static/404/404.html"), 'utf8'));
 
     app.use(function(req, res, next) {
         var ua = req.headers['user-agent'] ? req.headers['user-agent'] : '';
@@ -109,6 +120,14 @@ function main() {
 
     app.get("/[ac]/[^/]+[/]?", function(req, res) {
         serve_static_file(req, res, "./static/index.html");
+    });
+
+    app.get('*', function(req, res) {
+        res.send(template_404({
+            image_name: i404[i404_idx],
+            url: req.url
+        }));
+        i404_idx = (i404_idx + 1) % i404.length;
     });
 
     ds.set_db_name(opts.db);
